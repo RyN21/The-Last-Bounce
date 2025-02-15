f36e965acf1f7eb59e9d710f8081d60e7b7fc52d
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-02-15 16:19:19 -0700

    Add potential architect for game by adding potential files and folders

---
+++ config/settings.rb
@@ -0,0 +1,12 @@
+module Config
+  # Window Settings
+  WINDOW_WIDTH  = 800
+  WINDOW_HEIGHT = 600
+  FULLSCREEN    = false
+  CAPTION       = "The Last Bounce"
+
+  # Grid Settings
+  CELL_SIZE     = 50
+  GRID_ROWS     = WINDOW_HEIGHT / CELL_SIZE
+  GRID_COLS     = WINDOW_WIDTH / CELL_SIZE
+end

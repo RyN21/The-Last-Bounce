1f1fcd7523d25bd8a56aff0f7035e2c72293071c
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-04-08 05:44:18 -0600

    Add menu window that allows user to scroll through options

--- lib/scenes/game_window.rb
+++ lib/scenes/game_window.rb
@@ -10,20 +10,22 @@ class GameWindow
   WIDTH  = Config::WINDOW_WIDTH
   HEIGHT = Config::WINDOW_HEIGHT
   COLOR  = Gosu::Color.rgba(120, 100, 255, 255)
+  MENUCOLOR = Gosu::Color.rgba(10, 10, 10, 225)
 
   def initialize(state_manager)
-    @state_manager = state_manager
-    @map           = Map.new("assets/maps/1.txt")
-    @m1            = Gosu::Image.new("assets/images/mountains_1.png")
-    @m2            = Gosu::Image.new("assets/images/mountains_2.png")
-    @m3            = Gosu::Image.new("assets/images/mountains_3.png")
-    @font          = Gosu::Font.new(30)
-    @paddle        = Paddle.new(200, 550, @map)
-    @ball          = Ball.new(225, 300, @paddle, @map)
-    @paused        = false
-    @pause_pressed = false
-    @paused_menu   = false
-    @menu_options  = ["Continue", "Restart", "Quit"]
+    @state_manager  = state_manager
+    @map            = Map.new("assets/maps/1.txt")
+    @m1             = Gosu::Image.new("assets/images/mountains_1.png")
+    @m2             = Gosu::Image.new("assets/images/mountains_2.png")
+    @m3             = Gosu::Image.new("assets/images/mountains_3.png")
+    @font           = Gosu::Font.new(30)
+    @paddle         = Paddle.new(200, 550, @map)
+    @ball           = Ball.new(225, 300, @paddle, @map)
+    @paused         = false
+    @pause_pressed  = false
+    @paused_menu    = false
+    @menu_options   = ["Continue", "Restart", "Quit"]
+    @menu_opt_index = 0
     @camera_x = @camera_y = 0
   end
 
@@ -52,14 +54,19 @@ class GameWindow
 
   def draw
     Gosu.draw_rect(0, 0, WIDTH, HEIGHT, COLOR, z = 0)
-    # Gosu.translate(-@camera_x * 0.2, 0) { @m1.draw(-150, 130, 0, 4, 4) }
-    # Gosu.translate(-@camera_x * 0.5, 0) { @m2.draw(-150, 170, 0, 4, 4) }
-    # Gosu.translate(-@camera_x * 0.65, 0) { @m3.draw(-150, 375, 0, 4, 4) }
     @map.draw(@camera_x, @camera_y, WIDTH, HEIGHT)
-    @font.draw_text("Game Paused", 225, 300, 0, 2, 2) if @paused
     Gosu.translate(-@camera_x, -@camera_y) do
       @paddle.draw
       @ball.draw
+    end
+    if @paused
+      Gosu.draw_rect(200, 150, 400, 350, MENUCOLOR)
+      @font.draw_text("Game Paused", 230, 180, 0, 2, 2)
+      @menu_options.each_with_index do |option, index|
+        shift = index * 50
+        color = index == @menu_opt_index ? Gosu::Color.argb(0xff_ff00ff) : Gosu::Color::WHITE
+        @font.draw_text(option, 275, 275 + shift, 1, 1, 1, color)
+      end
     end
   end
 
@@ -68,5 +75,36 @@ class GameWindow
     when Gosu::KB_ESCAPE
       @state_manager.switch_to(Menu.new(@state_manager))
     end
+    if @paused
+      case id
+      when Gosu::KB_UP
+        @menu_opt_index = (@menu_opt_index - 1) % @menu_options.size
+      when Gosu::KB_DOWN
+        @menu_opt_index = (@menu_opt_index + 1) % @menu_options.size
+      when Gosu::KB_RETURN
+        handle_menu_optino_selection
+      end
+    end
+  end
+
+  def handle_menu_optino_selection
+    case @menu_opt_index
+    when 0
+      @paused = false
+    when 1
+      @paused = false
+    when 2
+      @paused = false
+    end
   end
 end
+
+
+
+
+
+# def draw_rectend
+# Gosu.translate(-@camera_x * 0.2, 0) { @m1.draw(-150, 130, 0, 4, 4) }
+# Gosu.translate(-@camera_x * 0.5, 0) { @m2.draw(-150, 170, 0, 4, 4) }
+# Gosu.translate(-@camera_x * 0.65, 0) { @m3.draw(-150, 375, 0, 4, 4) }
+# end

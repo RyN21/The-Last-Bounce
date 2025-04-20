d5c116f958b12b333c9a8d4bcf2b7ad76f549493
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-04-20 09:56:02 -0600

    Trying to edit menu screens and level selection screeen

--- lib/scenes/game_window.rb
+++ lib/scenes/game_window.rb
@@ -26,11 +26,15 @@ class GameWindow
     @paused_menu     = false
     @menu_options    = ["Continue", "Restart", "Select Level", "Quit"]
     @lose_options    = ["Try Again", "Select Level", "Quit"]
-    @levels          = [1, 2, 3, 4]
+    @win_options     = ["Next Level", "Select Level", "Quit"]
+    # @unlocked_levels = [1]
+    # @locked_levels   = [2, 3, 4]
+    @levels          = [1, 2, 3, 4, "Back"]
     @level_index     = 0
     @level_screen    = false
     @menu_opt_index  = 0
     @lose_opt_index  = 0
+    @win_opt_index   = 0
     @level_opt_index = 0
     @camera_x = @camera_y = 0
   end
@@ -73,16 +77,16 @@ class GameWindow
         @font.draw_text(option, 275, 275 + shift, 1, 1, 1, color)
       end
     end
-    if @paused && @level_screen
+    if @level_screen
       Gosu.draw_rect(200, 150, 400, 350, MENUCOLOR)
       @font.draw_text("Select Level", 230, 180, 0, 2, 2)
       @levels.each_with_index do |level, index|
         shift = index * 50
         color = index == @level_index ? Gosu::Color.argb(0xff_ff00ff) : Gosu::Color::WHITE
-        @font.draw_text(level, 275, 275 + shift, 1, 1, 1, color)
+        @font.draw_text(level, 275, 250 + shift, 1, 1, 1, color)
       end
     end
-    if @ball.lose?
+    if @ball.lose? && !@level_screen
       Gosu.draw_rect(200, 150, 400, 350, MENUCOLOR)
       @font.draw_text("You Lose", 230, 180, 0, 2, 2)
       @lose_options.each_with_index do |option, index|
@@ -91,12 +95,12 @@ class GameWindow
         @font.draw_text(option, 275, 275 + shift, 1, 1, 1, color)
       end
     end
-    if @ball.win?
+    if @ball.win? && !@level_screen
       Gosu.draw_rect(200, 150, 400, 350, MENUCOLOR)
       @font.draw_text("Level Passed", 230, 180, 0, 2, 2)
-      @lose_options.each_with_index do |option, index|
+      @win_options.each_with_index do |option, index|
         shift = index * 50
-        color = index == @lose_opt_index ? Gosu::Color.argb(0xff_ff00ff) : Gosu::Color::WHITE
+        color = index == @win_opt_index ? Gosu::Color.argb(0xff_ff00ff) : Gosu::Color::WHITE
         @font.draw_text(option, 275, 275 + shift, 1, 1, 1, color)
       end
     end
@@ -109,36 +113,51 @@ class GameWindow
       @paused_menu   = @paused
       @pause_pressed = true
     end
-    if @paused
+    if @paused && !@level_screen
       case id
       when Gosu::KB_UP
-        @menu_opt_index = (@menu_opt_index - 1) % @menu_options.size if !@level_screen
-        @level_index = (@level_index - 1) % @levels.size if @level_screen
+        @menu_opt_index = (@menu_opt_index - 1) % @menu_options.size
+        # @level_index = (@level_index - 1) % @levels.size if @level_screen
       when Gosu::KB_DOWN
-        @menu_opt_index = (@menu_opt_index + 1) % @menu_options.size if !@level_screen
-        @level_index = (@level_index + 1) % @levels.size if @level_screen
+        @menu_opt_index = (@menu_opt_index + 1) % @menu_options.size
+        # @level_index = (@level_index + 1) % @levels.size if @level_screen
       when Gosu::KB_RETURN
-        case @level_screen
-        when false
-          handle_menu_optino_selection
-        when true
-          handle_level_screen_options
-        end
+        handle_menu_option_selection
       end
     end
-    if @ball.lose?
+    if @level_screen
+      case id
+      when Gosu::KB_UP
+        @level_index = (@level_index - 1) % @levels.size
+      when Gosu::KB_DOWN
+        @level_index = (@level_index + 1) % @levels.size
+      when Gosu::KB_RETURN
+        handle_level_screen_options
+      end
+    end
+    if @ball.lose? && !@level_screen
       case id
       when Gosu::KB_UP
         @lose_opt_index = (@lose_opt_index - 1) % @lose_options.size
       when Gosu::KB_DOWN
         @lose_opt_index = (@lose_opt_index + 1) % @lose_options.size
       when Gosu::KB_RETURN
-        handle_lose_optino_selection
+        handle_lose_option_selection
+      end
+    end
+    if @ball.win? && !@level_screen
+      case id
+      when Gosu::KB_UP
+        @win_opt_index = (@win_opt_index - 1) % @win_options.size
+      when Gosu::KB_DOWN
+        @win_opt_index = (@win_opt_index + 1) % @win_options.size
+      when Gosu::KB_RETURN
+        handle_win_option_selection
       end
     end
   end
 
-  def handle_menu_optino_selection
+  def handle_menu_option_selection
     case @menu_options[@menu_opt_index]
     when "Continue"
       @paused = false
@@ -151,19 +170,32 @@ class GameWindow
     end
   end
 
-  def handle_lose_optino_selection
+  def handle_lose_option_selection
     case @lose_options[@lose_opt_index]
     when "Try Again"
       @state_manager.switch_to(GameWindow.new(@state_manager, @level))
     when "Select Level"
-      @state_manager.switch_to(GameWindow.new(@state_manager, @level))
+      @level_screen = true
+    when "Quit"
+      @state_manager.switch_to(Menu.new(@state_manager))
+    end
+  end
+
+  def handle_win_option_selection
+    level = @level
+    case @win_options[@win_opt_index]
+    when "Next Level"
+      level += 1
+      @state_manager.switch_to(GameWindow.new(@state_manager, level))
+    when "Select Level"
+      @level_screen = true
     when "Quit"
       @state_manager.switch_to(Menu.new(@state_manager))
     end
   end
 
   def handle_level_screen_options
-    case @level_index
+    case @levels[@level_index]
     when 0
       @level = 1
       @level_screen = false
@@ -180,6 +212,8 @@ class GameWindow
       @level = 4
       @level_screen = false
       @state_manager.switch_to(GameWindow.new(@state_manager, @level))
+    when "Back"
+      @level_screen = false
     end
   end
 end
